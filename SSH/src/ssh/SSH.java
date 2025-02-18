c3052236ea588700c447d2c4d6aadac5f936dac7
airamdl <airamdl2001@gamil.com>
2025-02-18 18:58:36 +0000

    correcion

--- SSH/src/ssh/SSH.java
+++ SSH/src/ssh/SSH.java
@@ -8,13 +8,62 @@ package ssh;
  *
  * @author airam
  */
+import com.jcraft.jsch.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 public class SSH {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+      // Variables de configuración
+    private static final String USERNAME = "alumno";
+    private static final String HOST = "localhost";
+    private static final int PORT = 22;
+    private static final String PASSWORD = "Alumno1234";
+    private static final String COMMAND = "ls -la"; 
+
+     public static void main(String[] args) {
+        try {
+            SSHConnector sshConnector = new SSHConnector();
+            sshConnector.connect(USERNAME, PASSWORD, HOST, PORT);
+            sshConnector.executeCommand(COMMAND);
+            
+        } catch (JSchException | IllegalAccessException | IOException ex) {
+            ex.printStackTrace();
+            System.out.println(ex.getMessage());
+        } 
     }
     
+}
+
+
+class SSHConnector {
+    private Session session;
+    
+    public void connect(String username, String password, String host, int port) throws JSchException, IllegalAccessException {
+        if (this.session == null || !this.session.isConnected()) {
+            JSch jsch = new JSch();
+            this.session = jsch.getSession(username, host, port);
+            this.session.setPassword(password);
+            this.session.setConfig("StrictHostKeyChecking", "no");
+            this.session.connect();
+        } else {
+            throw new IllegalAccessException("Sesión SSH ya iniciada");
+        }
+    }
+    
+    public final void executeCommand(String command) throws IllegalAccessException, JSchException, IOException {
+        if (this.session != null && this.session.isConnected()) {
+            Channel channel = this.session.openChannel("exec");
+            ChannelExec channelExec = (ChannelExec) channel;
+            channelExec.setCommand(command);
+            channel.setInputStream(System.in);
+            channel.setOutputStream(System.out);
+            channel.connect(3 * 1000);
+            
+        } else {
+            throw new IllegalAccessException("No existe sesión SSH iniciada.");
+
+        }
+    }
 }
